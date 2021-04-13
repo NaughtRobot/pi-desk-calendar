8fbf269a35371baba5c4681438cc2d5ce7695554
Naught Robot <naughtrobot@pm.me>
2021-04-13 14:41:02 -0600

    WIP: Add Personal Top 10 Games

--- pi-desk-calendar.py
+++ pi-desk-calendar.py
@@ -2,6 +2,7 @@
 """Python Desktop Calendar."""
 
 import json
+import sys
 
 from datetime import date, datetime
 from time import sleep
@@ -12,6 +13,8 @@ import xmltodict
 from inky import InkyWHAT
 from PIL import Image, ImageFont, ImageDraw
 from font_fredoka_one import FredokaOne
+
+sys.getdefaultencoding()
 
 def read_config():
     """Read Calendar Config File."""
@@ -40,6 +43,83 @@ def get_hot_games():
         else:
             break
     return hot_games
+
+def weighted_average(rating, mean, plays):
+    """Calculate weighted average for game."""
+    rating, mean, plays = float(rating), float(mean), float(plays)
+    minimum_plays = 25
+    rating = (plays / (plays + minimum_plays)) * rating \
+        + (minimum_plays / (plays + minimum_plays)) * mean
+    return rating
+
+def multi_key_sort(items, columns):
+    """Sort dictionary based on multiple keys."""
+    comparers = [((itemgetter(col[1:].strip()), 1) if col.startswith('-') else
+                  (itemgetter(col.strip()), -1)) for col in columns]
+
+    def comparer(left, right):
+        for _fn, mult in comparers:
+            result = ((_fn(left) > _fn(right)) - (_fn(left) < _fn(right)))
+            if result:
+                return mult * result
+        return None
+
+    return sorted(items, key=cmp_to_key(comparer))
+
+def calculate_mean(collection):
+    """Calculate the mean ration for collection."""
+    ratings = []
+    for game in collection['items']['item']:
+        ratings.append(float(game['stats']['rating']['@value']))
+    mean = sum(ratings)/len(ratings)
+    return mean
+
+def get_collection():
+    """Get user's collection from BGG."""
+    collection = []
+    retry = 0
+    config = read_config()
+    username = config['bgg']['username']
+    baseurl = 'https://www.boardgamegeek.com/xmlapi2/'
+    url = baseurl + ('collection?username={}&own=1'
+                     '&rated=1&played=1&stats=1').format(username)
+    data = request_data(url)
+    doc = xmltodict.parse(data)
+    mean_rating = calculate_mean(doc)
+    try:
+        for game in doc['items']['item']:
+            title = game['name']['#text'].encode('utf-8').strip()
+            player_rating = game['stats']['rating']['@value']
+            plays = game['numplays']
+            rating = weighted_average(player_rating, mean_rating, plays)
+            COLLECTION.append({'name': title, 'player_rate': player_rating,
+                                'rating': rating, 'plays': plays})
+    except KeyError:
+        if retry < 5:
+            retry += 1
+            get_collection()
+        else:
+            collection = ("Could not fetch collection from BGG.")
+            return collection
+
+    collection = multikeysort(COLLECTION, ['rating', 'name'])
+
+    return collection
+
+def get_personal_top_ten():
+    """Get personal top ten games using weighted average."""
+    top_games = ""
+    rank = 1
+    collection = get_collection()
+    for game in collection:
+        top_games += "{0}{1}".format(str(rank).ljust(3), str(
+            game['name']).strip('b').strip('\'').strip('\"'))
+        if rank <= 9:
+            rank += 1
+        else:
+            break
+    return top_games
+
 
 def convention_countdown():
     """Calculate the number of days until a given convention."""
@@ -72,7 +152,6 @@ def dispaly_calendar_page(title, page_content, font_size):
     padding = 20
 
     today = date.today().strftime("%B %d, %Y")
-    today
 
     # Display Date Banner
     for y in range(0,(5 + date_font.getsize(today)[1])):
@@ -102,6 +181,8 @@ def dispaly_calendar_page(title, page_content, font_size):
     inky_display.show()
 
 if __name__ == "__main__":
-    dispaly_calendar_page("Top 10 Hot Games", get_hot_games(), 16)
-    sleep(30)
-    dispaly_calendar_page("Boad Game Convention Countdown", convention_countdown(), 18)
+    dispaly_calendar_page("Top 10 Hot Games", get_hot_games(), 15)
+    sleep(15)
+    dispaly_calendar_page("Boad Game Convention Countdown", convention_countdown(), 22)
+    sleep(15)
+    dispaly_calendar_page("Personal Top 10 Games", get_personal_top_ten(), 15)
