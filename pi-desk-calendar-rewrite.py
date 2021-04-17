099845f796f0de5bcac2cd835fbddf6c6e57fb23
Naught Robot <naughtrobot@pm.me>
2021-04-17 13:34:51 -0600

    WIP: Countdown Page / New Fonts

--- pi-desk-calendar-rewrite.py
+++ pi-desk-calendar-rewrite.py
@@ -15,7 +15,6 @@ import xmltodict
 
 from inky import InkyWHAT
 from PIL import Image, ImageFont, ImageDraw
-from font_fredoka_one import FredokaOne
 
 sys.getdefaultencoding()
 
@@ -31,50 +30,55 @@ def request_data(url):
     data = requests.get(url)
     return xmltodict.parse(data.content)
 
+
+def display_calendar_page(font_size, page_title, page_content):
+    """Display calendar page."""
+    today = date.today().strftime("%B %d, %Y")
+    inky_display = InkyWHAT("red")
+    inky_display.set_border(inky_display.BLACK)
+    img = Image.new("P", (inky_display.WIDTH, inky_display.HEIGHT))
+    draw = ImageDraw.Draw(img)
+
+    regular_font = "fonts/Monoid-Reqular.ttf"
+    bold_font = "fonts/Monoid-Bold.ttf"
+    italic_font = "fonts/Monoid-Italic.ttf"
+
+    date_font = ImageFont.truetype(bold_font, 32)
+    title_font = ImageFont.truetype(italic_font, 23)
+    page_content_font = ImageFont.truetype(regular_font, font_size)
+    
+    padding = 20
+
+    # Display Date Banner
+    for y in range(0,(5 + date_font.getsize(today)[1])):
+        for x in range(0, inky_display.WIDTH):
+            img.putpixel((x,y), inky_display.RED)
+    w,h = date_font.getsize(today)
+    x_today = (inky_display.WIDTH / 2 ) - (w / 2)
+    y_today = 0
+    draw.text((x_today, y_today), today, inky_display.WHITE, date_font)
+    
+    # Display Page Title
+    w,h = title_font.getsize(page_title)
+    x_title = (inky_display.WIDTH / 2) - (w / 2)
+    y_title = y_today + padding + h
+    draw.text((x_title, y_title), page_title, inky_display.BLACK, title_font)
+
+    #Display Page Content
+    w,h = page_content_font.getsize(page_content)
+    x_content = 5
+    y_content = y_title + padding + h
+    draw.text((x_content, y_content), page_content, inky_display.BLACK, page_content_font)
+
+    # For Testing
+    flipped = img.rotate(180)
+
+    inky_display.set_image(flipped)
+    inky_display.show()
+
+
 def display_hot_games():
     """Display top ten games on Board Game Geek hot games list."""
-
-    def display_calendar_page(page_content):
-        """Display Hot Games calendar page."""
-        today = date.today().strftime("%B %d, %Y")
-        page_title = "Top 10 Hot Games"
-        inky_display = InkyWHAT("red")
-        inky_display.set_border(inky_display.BLACK)
-        img = Image.new("P", (inky_display.WIDTH, inky_display.HEIGHT))
-        draw = ImageDraw.Draw(img)
-
-        date_font = ImageFont.truetype(FredokaOne, 32)
-        title_font = ImageFont.truetype(FredokaOne, 23)
-        page_content_font = ImageFont.truetype(FredokaOne, 15)
-        
-        padding = 20
-
-        # Display Date Banner
-        for y in range(0,(5 + date_font.getsize(today)[1])):
-            for x in range(0, inky_display.WIDTH):
-                img.putpixel((x,y), inky_display.RED)
-        w,h = date_font.getsize(today)
-        x_today = (inky_display.WIDTH / 2 ) - (w / 2)
-        y_today = 0
-        draw.text((x_today, y_today), today, inky_display.WHITE, date_font)
-        
-        # Display Page Title
-        w,h = title_font.getsize(page_title)
-        x_title = (inky_display.WIDTH / 2) - (w / 2)
-        y_title = y_today + padding + h
-        draw.text((x_title, y_title), page_title, inky_display.BLACK, title_font)
-
-        #Display Page Content
-        w,h = page_content_font.getsize(page_content)
-        x_content = 5
-        y_content = y_title + padding + h
-        draw.text((x_content, y_content), page_content, inky_display.BLACK, page_content_font)
-
-        # For Testing
-        flipped = img.rotate(180)
-
-        inky_display.set_image(flipped)
-        inky_display.show()
 
     hot_games_api = "https://www.boardgamegeek.com/xmlapi2/hot?boardgames"
     hot_games_data = request_data(hot_games_api)
@@ -88,7 +92,33 @@ def display_hot_games():
         else:
             break
 
-    display_calendar_page(hot_games_list)
+    display_calendar_page(15, "Top 10 Hot Games", hot_games_list)
+
+
+def display_convention_countdown():
+    """Calculate the number of days until a given convention."""
+    
+    def calc_date_delta(convention_start_date):
+        """Calculate delta between two dates."""
+        today = date.today()
+        delta = convention_start_date - today
+        return delta.days
+    
+    convention_data = read_config()
+    convention_list = ""
+    for convention in convention_data['game_conventions']:
+        days_between = calc_date_delta(datetime.strptime(convention['start_date'], "%Y-%m-%d").date())
+        if days_between < 0:
+            continue
+        elif days_between == 1:
+            convention_list += "{} day until {}\n\n".format(days_between, convention['name'])
+        elif days_between == 0:
+            convention_list += "{} starts today!\n\n".format(convention['name'])
+        else:
+            convention_list += "{} days until {}\n\n".format(days_between, convention['name'])
+    
+    display_calendar_page(15, "Upcoming Gaming Conventions", convention_list)
 
 if __name__ == "__main__":
     display_hot_games()
+    display_convention_countdown()
